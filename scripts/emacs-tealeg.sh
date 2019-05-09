878a73a6f599c3bbd2a5c34c7419fc66a48d5b2d
Aaron Lee <alee@fastly.com>
2019-05-09 18:55:16 -0500

    At least it compiles

--- scripts/emacs-tealeg.sh
+++ scripts/emacs-tealeg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-EMACSVERSION=25.2
+EMACSVERSION=26.2
 export EMACSDATA=$SNAP/share/emacs/$EMACSVERSION/etc
 export EMACSLOADPATH=$SNAP/share/emacs/site-lisp:$SNAP/share/emacs/$EMACSVERSION/lisp/
 export INFOPATH=$SNAP/share/info/
